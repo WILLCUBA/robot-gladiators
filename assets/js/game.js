d09e828b9608837ea738bb7e82acb7cb210ab5fc
Will <wilito1210@gmail.com>
2022-01-03 23:46:17 -0500

    end and star game function

--- assets/js/game.js
+++ assets/js/game.js
@@ -69,7 +69,21 @@ var fight = function(enemyName) {
   }
 };
 var endGame = function() {
-  window.alert("The game has now ended. Let's see how you did!")
+  // if player is still alive, player wins!
+  if (playerHealth > 0) {
+    window.alert("Great job, you have survived the game! You now have a score")
+  }
+  else {
+    window.alert("You have lost your robot in battle.")
+  }
+  var playAgainConfirm = window.confirm("Would you like to play again?");
+
+  if (playAgainConfirm) {
+    startGame()
+  }
+  else {
+    window.alert("thank you for playing")
+  }
 }
 
 // fight each enemy-robot by looping over them and fighting them one at a time
@@ -80,29 +94,23 @@ var startGame = function() {
 
   for (var i = 0; i < enemyNames.length; i++) {
   // if player is still alive, keep fighting
-  if (playerHealth > 0) {
-    // let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
-    window.alert('Welcome to Robot Gladiators! Round ' + (i + 1));
+    if (playerHealth > 0) {
+  
+      window.alert('Welcome to Robot Gladiators! Round ' + (i + 1));
 
-    // pick new enemy to fight based on the index of the enemyNames array
-    var pickedEnemyName = enemyNames[i];
+      var pickedEnemyName = enemyNames[i];
 
-    // reset enemyHealth before starting new fight
-    enemyHealth = 50;
+      enemyHealth = 50;
 
-    // use debugger to pause script from running and check what's going on at that moment in the code
-    // debugger;
-
-    // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
-    fight(pickedEnemyName);
-  }
-  // if player isn't alive, stop the game
+   
+      fight(pickedEnemyName);
+    }
   else {
     window.alert('You have lost your robot in battle! Game Over!');
     break;
     }
+  endGame()
   }
-  startGame()
 }
 
 startGame();
